2f90f28c432fd0f97fe5a28b45124490c6f86a31
SURAJ <90304648+SURAJ-SHARMA27@users.noreply.github.com>
2023-11-18 12:35:50 +0530

    loading spinner added

---
+++ src/components/LoadingSpinner.jsx
@@ -0,0 +1,15 @@
+// LoadingSpinner.jsx
+
+import React from 'react';
+import '../components/stylecss/Login.css';
+
+
+const LoadingSpinner = () => {
+  return (
+    <div class="loadingio-spinner-spinner-dtd5b1kjoqi"><div class="ldio-9r9i911zkwt">
+    <div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div>
+    </div></div>
+  );
+};
+
+export default LoadingSpinner;
