fe4b1690db12f16fa937d6d77122c04395e4604d
SURAJ <90304648+SURAJ-SHARMA27@users.noreply.github.com>
2023-11-19 14:12:49 +0530

    vite.config is used

--- vite.config.js
+++ vite.config.js
@@ -3,18 +3,18 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 
-const baseUrl = `https://odia-preneur.onrender.com`
+const useit = `https://odia-preneur.onrender.com`
 
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/register': baseUrl,
-      '/signin': baseUrl,
-      '/about': baseUrl,
-      '/contact': baseUrl,
-      '/registeredteams': baseUrl,
-      '/logout': baseUrl
+      '/register': useit,
+      '/signin': useit,
+      '/about': useit,
+      '/contact': useit,
+      '/registeredteams': useit,
+      '/logout': useit
     },
   },
 });
